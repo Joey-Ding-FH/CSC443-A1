1a68cc38f3a978beb2b29b6ba841de0f2f1c00dd
Zhou Lisa <g2zhouli@cdf.toronto.edu>
2014-10-13 19:29:55 -0400

    Select2 changes I think?

--- select2.cc
+++ select2.cc
@@ -3,6 +3,9 @@
 #include <sys/types.h>
 #include <dirent.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <errno.h>
 #include "library.h"
 
 
@@ -14,26 +17,21 @@ int main(int argc, char *argv[]){
 		exit(1);
 	}
 
-	//assuming: colstore_name is a directory that exists
-
-	if (chdir(argv[1]) == -1)
-	{
-		fprintf(stderr, "Could not navigate to storage directory: %s", argv[2]);
-		exit(1);
-	}
-
-	DIR *colStore = opendir(argv[1]);
-	if (colStore == NULL)
-	{
-		fprintf(stderr, "Could not open directory %s for reading.", argv[2]);
-		exit(1);
-
-	}
-
+	char *dirName = argv[1];
 	char *fileName = argv[2];
 	char *startVal = argv[3];
 	char *endVal = argv[4];
 	int pageSize = atoi(argv[5]);
+
+	//assuming: colstore_name is a directory that exists
+	DIR *colStore = opendir(dirName);
+	if (colStore == NULL)
+	{
+		fprintf(stderr, "Could not open directory %s for reading. Exiting with error: %d\n",
+			dirName, errno);
+		exit(1);
+
+	}
 
 	struct dirent *entr = readdir(colStore);
 
@@ -50,20 +48,35 @@ int main(int argc, char *argv[]){
 		exit(1);
 	}
 
+	if (chdir(dirName) == -1)
+	{
+		fprintf(stderr, "Could not navigate to storage directory: %s\n", dirName);
+		exit(1);
+	}
+
 	Heapfile *hpFile = new Heapfile();
 	init_heapfile(hpFile, pageSize, fopen(fileName, "rb"));
 
+	cout << "Heapfile initialized for attributeId: " << fileName << endl;
+
 	RecordIterator *recIter = new RecordIterator(hpFile);
+	
+	int comparelen = (strlen(startVal) < ATTRIBUTE_SIZE) ? strlen(startVal)  : ATTRIBUTE_SIZE;
+	comparelen = (strlen(endVal) < comparelen) ? strlen(endVal) : comparelen;
 
 	while (recIter->hasNext())
 	{
+		cout << "Record exists" << endl;
 		Record rec = recIter->next();
 		if(rec.size() != 1)
 		{
 			//print some sort of error..or just keep going?
 			exit(1);
 		}
-		if (memcmp(startVal, rec[0], ATTRIBUTE_SIZE) == 0)
+		cout << "Record is" << rec[0] << endl;
+
+		if (memcmp(startVal, rec[0], ATTRIBUTE_SIZE) <= 0
+			&& memcmp(endVal, rec[0], ATTRIBUTE_SIZE) >= 0)
 		{
 			char buf[5]; 
 			fixed_len_read(buf, 5, &rec);
